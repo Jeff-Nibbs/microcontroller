cb4d33724a3d658388caa694bbd5f24353d98acf
Jeffrey Nibbs <djcountry100@gmail.com>
2025-05-05 03:55:35 -0700

    cute fcae guy comming

--- mr_mans/mr_mans.ino
+++ mr_mans/mr_mans.ino
@@ -1,24 +1,178 @@
-#include <Arduino.h>
+#include <SPI.h>
 #include <U8g2lib.h>
 
-#ifdef U8X8_HAVE_HW_SPI
-#include <SPI.h>
-#endif
-#ifdef U8X8_HAVE_HW_I2C
-#include <Wire.h>
-#endif
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
-void setup(void) {
+//be sure to initialize with your display.  see https://github.com/olikraus/u8g2/wiki/setup_tutorial
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0);
+
+int COLOR_WHITE = 1;
+int COLOR_BLACK = 0;
+
+int eye_width = 35;
+int eye_height = 41;
+int eye_radius = 7;
+int eye_y = 10;
+
+int left_eye_x = 19;
+int right_eye_x = 74;
+
+int mouth_x = 57;
+int mouth_y = 50;
+int mouth_width = 15;
+int mouth_height = 6;
+int mouth_radius = 1;
+
+void clearDisplay()
+{
+  u8g2.clearBuffer();
+}
+
+void sendDisplay() {
+  u8g2.sendBuffer();
+}
+
+// Resets up main face
+void resetMainFace() {
+  clearDisplay();
+  // left eye
+  u8g2.drawRBox(left_eye_x, eye_y, eye_width, eye_height, eye_radius);
+  // right eye
+  u8g2.drawRBox(right_eye_x, eye_y, eye_width, eye_height, eye_radius);
+  // mouth
+  u8g2.drawRBox(mouth_x, mouth_y, mouth_width, mouth_height, mouth_radius);
+  sendDisplay();
+}
+
+void plusPlus(int r1, int r2) {
+  clearDisplay();
+  // left eye
+  u8g2.drawRBox(left_eye_x + r1, eye_y + r2, eye_width, eye_height, eye_radius);
+  // right eye
+  u8g2.drawRBox(right_eye_x + r1, eye_y + r2, eye_width, eye_height, eye_radius);
+  // mouth
+  u8g2.drawRBox(mouth_x + r1, mouth_y + r2, mouth_width, mouth_height, mouth_radius);
+  sendDisplay();
+}
+
+void nanPlus(int r1, int r2) {
+  clearDisplay();
+  // left eye
+  u8g2.drawRBox(left_eye_x, eye_y + r2, eye_width, eye_height, eye_radius);
+  // right eye
+  u8g2.drawRBox(right_eye_x, eye_y + r2, eye_width, eye_height, eye_radius);
+  // mouth
+  u8g2.drawRBox(mouth_x, mouth_y + r2, mouth_width, mouth_height, mouth_radius);
+  sendDisplay();
+}
+
+void negPlus(int r1, int r2) {
+  clearDisplay();
+  // left eye
+  u8g2.drawRBox(left_eye_x - r1, eye_y + r2, eye_width, eye_height, eye_radius);
+  // right eye
+  u8g2.drawRBox(right_eye_x - r1, eye_y + r2, eye_width, eye_height, eye_radius);
+  // mouth
+  u8g2.drawRBox(mouth_x - r1, mouth_y + r2, mouth_width, mouth_height, mouth_radius);
+  sendDisplay();
+}
+
+void negNan(int r1, int r2) {
+  clearDisplay();
+  // left eye
+  u8g2.drawRBox(left_eye_x - r1, eye_y, eye_width, eye_height, eye_radius);
+  // right eye
+  u8g2.drawRBox(right_eye_x - r1, eye_y, eye_width, eye_height, eye_radius);
+  // mouth
+  u8g2.drawRBox(mouth_x - r1, mouth_y, mouth_width, mouth_height, mouth_radius);
+  sendDisplay();
+}
+
+void negNeg(int r1, int r2) {
+  clearDisplay();
+  // left eye
+  u8g2.drawRBox(left_eye_x - r1, eye_y - r2, eye_width, eye_height, eye_radius);
+  // right eye
+  u8g2.drawRBox(right_eye_x - r1, eye_y - r2, eye_width, eye_height, eye_radius);
+  // mouth
+  u8g2.drawRBox(mouth_x - r1, mouth_y - r2, mouth_width, mouth_height, mouth_radius);
+  sendDisplay();
+}
+
+void nanNeg(int r1, int r2) {
+  clearDisplay();
+  // left eye
+  u8g2.drawRBox(left_eye_x, eye_y - r2, eye_width, eye_height, eye_radius);
+  // right eye
+  u8g2.drawRBox(right_eye_x, eye_y - r2, eye_width, eye_height, eye_radius);
+  // mouth
+  u8g2.drawRBox(mouth_x, mouth_y - r2, mouth_width, mouth_height, mouth_radius);
+  sendDisplay();
+}
+
+void plusNeg(int r1, int r2) {
+  clearDisplay();
+  // left eye
+  u8g2.drawRBox(left_eye_x + r1, eye_y + r2, eye_width, eye_height, eye_radius);
+  // right eye
+  u8g2.drawRBox(right_eye_x + r1, eye_y + r2, eye_width, eye_height, eye_radius);
+  // mouth
+  u8g2.drawRBox(mouth_x + r1, mouth_y + r2, mouth_width, mouth_height, mouth_radius);
+  sendDisplay();
+}
+
+void plusNan(int r1, int r2) {
+  clearDisplay();
+  // left eye
+  u8g2.drawRBox(left_eye_x + r1, eye_y, eye_width, eye_height, eye_radius);
+  // right eye
+  u8g2.drawRBox(right_eye_x + r1, eye_y, eye_width, eye_height, eye_radius);
+  // mouth
+  u8g2.drawRBox(mouth_x + r1, mouth_y, mouth_width, mouth_height, mouth_radius);
+  sendDisplay();
+}
+
+void move() {
+  int rand1 = random(18);
+  int rand2 = random(9);
+  int picker = random(8);
+
+  if (picker == 0) {
+    nanNeg(rand1, rand2);
+  } else if (picker == 1) {
+    plusNeg(rand1, rand2);
+  } else if (picker == 2) {
+    plusNan(rand1, rand2);
+  } else if (picker == 3) {
+    plusPlus(rand1, rand2 % 7);
+  } else if (picker == 4) {
+    nanPlus(rand1, rand2 % 7);
+  } else if (picker == 5) {
+    negPlus(rand1, rand2 % 7);
+  } else if (picker == 6) {
+    negNan(rand1, rand2);
+  } else if (picker == 7) {
+    negNeg(rand1, rand2);
+  }
+}
+
+void setup() {
+  u8g2.setI2CAddress(0x78);
+  u8g2.setDisplayRotation(U8G2_R0);
   u8g2.begin();
+  // set the face
+  resetMainFace();
+  delay(3000);
 }
 
-void loop(void) {
-  u8g2.clearBuffer();					// clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
-  u8g2.drawXBMP(0, 0, 128, 64, )
-  u8g2.sendBuffer();					// transfer internal memory to the display
-  delay(1000);  
+void loop() {
+  int looks = random(1, 4);
+  for (int i = 0; i < looks; i++) {
+    move();
+    delay(looks * 1000);
+  }
+  resetMainFace();
+  delay(looks * 1000);
 }
-
